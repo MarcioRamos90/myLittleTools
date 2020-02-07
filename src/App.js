551d597d86dc9f4c23d103c0a54cbeb073229c9a
Marcio Ramos <marcioramos@alumy.com>
2020-02-06 23:09:07 -0300

    Iniciando app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [textInput, setTextInput] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        name="texto"
+        onChange={text => setTextInput(text.target.value)}
+        value={textInput}
+      ></input>
+      {textInput.toUpperCase()}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
